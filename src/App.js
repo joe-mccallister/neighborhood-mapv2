65555b8c260a86f35ecc765e7df6868fd2d0b1aa
Joe McCallister <macattack1380@gmail.com>
2018-11-02 09:49:53 -0600

    refactored application -failed to commit previous and mass commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,104 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import MapContainer from './Components/MapContainer';
+import MapNav from"./Components/MapNav";
+import * as FourSquareAPI from './APIs/FourSquare';
 
 class App extends Component {
+  state = {
+    places: [
+      { 
+        name: "Epic Brewing Company",
+        location: {
+          lat: 39.763428, 
+          lng: -104.980830
+        },
+        img: '',
+        likes: ''
+      },
+      { 
+        name: "Bierstadt Lagerhaus",
+        location: {
+          lat: 39.762973, 
+          lng: -104.983303
+        },
+        img: '',
+        likes: ''
+      },
+      { 
+        name: "Ratio Beerworks",
+        location: {
+          lat: 39.761842, 
+          lng: -104.980435 
+        },
+        img: '',
+        likes: ''
+      },
+      { 
+        name: "Stem Ciders",
+        location: {
+          lat: 39.761830, 
+          lng: -104.983106
+        },
+        img: '',
+        likes: ''
+      },
+      { 
+        name: "Our Mutual Friend Brewing",
+        location: {
+          lat: 39.760769, 
+          lng: -104.981376
+        },
+        img: '',
+        likes: ''
+      }
+    ],
+    currentPlaces: [],
+    requestAvailable: true
+};
+
+componentDidMount() {
+  this.getFourSquareData();
+}
+
+  // Fetch FourSquare data from API
+  getFourSquareData = () => {
+    const newPlaces = this.state.places.map((place) => {
+      const size = 150
+      FourSquareAPI.getFourSquareVenueID(place.location.lat, place.location.lng, place.name)
+        .then((venueId) => {
+          FourSquareAPI.getFourSquareVenueInfo(venueId)
+            .then((venueInfo) => {
+              place.likes = venueInfo.likes.count
+              place.img = venueInfo.bestPhoto.prefix + size + venueInfo.bestPhoto.suffix
+            })
+            .catch(() => this.setState({ requestAvailable: false })
+        )})
+        .catch((e) => alert(e));
+      return place;
+    });
+    this.setState({ currentPlaces: newPlaces });
+  }
+  
+  // Filter a new array of current places based on user query
+  filterPlaces = (query) => {  
+    if (!query) {
+      this.setState({ currentPlaces: [] });
+    }
+    const filteredPlaces = this.state.places.filter((place) => place.name.toLowerCase().includes(query.toLowerCase()));  
+    this.setState({ currentPlaces: filteredPlaces });
+  }
+
+  // Set active marker when clicking list item
+  setActiveMarker = (marker) => {
+    document.querySelector(`[title="${marker}"]`).click();
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <MapNav places={this.state.currentPlaces} onQuery={this.filterPlaces} setActiveMarker={this.setActiveMarker}/>
+        <MapContainer places={this.state.currentPlaces} centerCoords={this.state.places[0].location} activeMarker={this.state.activeMarker} showingInfoWindow={this.state.showingInfoWindow} requestAvailable={this.state.requestAvailable}/>  
       </div>
     );
   }
---
+++ src/Components/MapNav.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+import '../App.css';
+
+class MapNav extends Component {
+    state = {
+        hamburgerActive: false,
+        query: ''
+    }
+
+    //Toggle Sidebar Menu (filter/query)
+    onHamburgerClick = () => {
+        const sidebar = document.querySelector('.map-sidebar');
+        if (this.state.hamburgerActive) {
+            sidebar.style.transform = 'translateX(-250px)'
+            this.setState({ hamburgerActive: false });
+        }
+        else {
+            sidebar.style.transform = 'translateX(0px)'
+            this.setState({ hamburgerActive: true});
+        }
+    }
+
+    render () {
+        return(
+            <div>
+        <div className="map-nav-container">
+          <nav className="map-nav">
+            <button aria-label='Hamburger Menu' tabIndex='0' className="hamburger-container" onClick={this.onHamburgerClick}> 
+              <div className="hamburger-bar"></div>
+              <div className="hamburger-bar"></div>
+              <div className="hamburger-bar"></div>
+            </button>
+            <div className="nav-title">
+              <h1>Popular Comedy Clubs</h1>
+              <h4>Powered by FourSquare</h4>
+            </div>
+          </nav>
+        </div>
+        <div className="map-sidebar">
+            <input aria-label='Search Filter' className="sidebar-input" onChange={(e) => this.props.onQuery(e.target.value)}></input>
+            <ul>
+              {this.props.places.map((place, index) => {
+                return <li 
+                tabIndex='0' 
+                role='button' 
+                aria-label='Place Location' 
+                key={index} 
+                onClick={() => this.props.setActiveMarker(place.name)}>{place.name}
+                </li>
+              })}
+            </ul>
+          </div>
+        </div>
+        )
+    }
+}
+
+export default MapNav
