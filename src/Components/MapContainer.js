ede87b5cc4c7f1416017b53e3726838625609c70
Joe McCallister <macattack1380@gmail.com>
2018-11-02 10:01:30 -0600

    change to container

---
+++ src/Components/MapContainer.js
@@ -0,0 +1,92 @@
+import React, { Component } from 'react';
+import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
+
+class MapContainer extends Component {
+  state = {
+    bounds: {},
+    selectedPlace: {},
+    likes: '',
+    img: '',
+    activeMarker: {},
+    showingInfoWindow: true,
+    center: {}
+  };
+
+  componentDidMount() {
+    this.setBounds();
+    this.setState({ center: this.props.centerCoords.location })
+  }
+
+  // Set map boundaries
+  setBounds = () => {
+    const bounds = new this.props.google.maps.LatLngBounds();
+    for (let place of this.props.places) {
+      bounds.extend(place.location);
+    }
+    this.setState({ bounds });
+  }
+
+  // InfoWindow on marker click
+  onMarkerClick = (props, marker) => {
+    const place = this.props.places.filter((place) => place.name === props.title)
+    this.setState({
+      showingInfoWindow: true,
+      activeMarker: marker,
+      selectedPlace: place[0],
+    });
+  }
+
+  render() {
+    const mapStyle = {
+      width: '100%',
+      height: '100%'
+    };
+    console.log(this.props.requestAvailable)
+
+    return (
+      <div>
+        <Map 
+          google={this.props.google} 
+          zoom={14} 
+          style={mapStyle} 
+          center={this.state.center}
+          bounds={this.state.bounds}
+        >
+          {this.props.places.map((place, index) => 
+            <Marker 
+              key={index}
+              name={place.name}
+              title={place.name}
+              position={{lat: place.location.lat, lng: place.location.lng}}
+              onClick={this.onMarkerClick}
+              animation={this.state.activeMarker.name === place.name &&this.props.google.maps.Animation.BOUNCE}
+            />
+          )}
+          <InfoWindow
+            marker={this.state.activeMarker}
+            visible={this.state.showingInfoWindow}
+
+          >
+            { !this.props.requestAvailable ? (
+              <div>
+                <h1>{this.state.selectedPlace.name}</h1>
+                <h3>Exceeded FourSquare API limit</h3>
+              </div>
+            ) : (
+              <div>
+                <h1>{this.state.selectedPlace.name}</h1>
+                <img src={this.state.selectedPlace.img} alt={this.state.selectedPlace.name}/>
+                <h3>Likes: {this.state.selectedPlace.likes}</h3>
+              </div>
+            )
+          }
+          </InfoWindow>
+        </Map>
+      </div>
+    )
+  }
+}
+
+export default GoogleApiWrapper({
+  apiKey: 'AIzaSyBVuYwtERyg82CFa4CH_NmIOlPBZBMxsfs'
+})(MapContainer);
